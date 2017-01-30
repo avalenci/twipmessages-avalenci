d32ff256cdcbc67f4016a92a5d20f2eb12595351
avalenci <ansmeew@gmail.com>
2017-01-30 14:30:19 -0500

    Initial commit

---
+++ src/com/company/Message.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Message {
+    public Message(String n, String txt, String time) {
+        number = n;
+        text = txt;
+    }
+    String number;
+    String text;
+    String timeRecieved;
+}
